67cbb662c946823d2e008b308d8e7a473756d793
twoeightdev <twoeightdev@gmail.com>
2024-04-25 20:53:45 +0800

    Refactor qtile configuration

--- .config/qtile/spotify.py
+++ .config/qtile/spotify.py
@@ -32,29 +32,32 @@ class Spotify(base.ThreadPoolText):
     """
     A widget to interact with spotify via dbus.
     """
+
     defaults = [
-        ("play_icon", ' ', "icon to display when playing music"),
-        ("pause_icon", '', "icon to display when music paused"),
+        ("play_icon", "", "icon to display when playing music"),
+        ("pause_icon", "", "icon to display when music paused"),
         ("update_interval", 0.5, "polling rate in seconds"),
-        ("format", "{artist} {icon} {track}",
-         "Spotify display format")
+        ("format", "{artist} {icon} {track}", "Spotify display format"),
     ]
 
     def __init__(self, **config):
-        base.ThreadPoolText.__init__(self, text='', **config)
+        base.ThreadPoolText.__init__(self, text="", **config)
         self.add_defaults(Spotify.defaults)
-        self.add_callbacks({
-            'Button3': self.go_to_spotify,
-            'Button1': self.toggle_music,
-        })
+        self.add_callbacks(
+            {
+                "Button3": self.go_to_spotify,
+                "Button1": self.toggle_music,
+            }
+        )
 
     def _is_proc_running(self, proc_name: str) -> bool:
         # create regex pattern to search for to avoid similar named processes
-        pattern = proc_name + '$'
+        pattern = proc_name + "$"
 
         # pgrep will return a string of pids for matching processes
-        proc_out = run(['pgrep', '-fli', pattern],
-                       capture_output=True).stdout.decode("utf-8")
+        proc_out = run(["pgrep", "-fli", pattern], capture_output=True).stdout.decode(
+            "utf-8"
+        )
 
         # empty string means nothing started
         if proc_out == "":
@@ -67,8 +70,8 @@ class Spotify(base.ThreadPoolText):
         if none exists then we will spawn an instance on the current group
         """
         # spawn spotify if not already running
-        if not self._is_proc_running('spotify'):
-            self.qtile.cmd_spawn('spotify', shell=True)
+        if not self._is_proc_running("spotify"):
+            self.qtile.cmd_spawn("spotify", shell=True)
             return
 
         all_groups: List[_Group] = self.qtile.groups
@@ -76,7 +79,7 @@ class Spotify(base.ThreadPoolText):
         for group in all_groups:
             info = group.info()
             # get a list of windows for the group.  We look for 'Spotify here'
-            windows = info['windows']
+            windows = info["windows"]
             name = group.name
             if "Spotify" in windows:
                 # switch to 'name' group
@@ -98,54 +101,64 @@ class Spotify(base.ThreadPoolText):
         return self.format.format(**vars)
 
     def toggle_music(self):
-        run("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause", shell=True)
+        run(
+            "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause",
+            shell=True,
+        )
 
     def get_proc_output(self, proc: CompletedProcess) -> str:
-        if proc.stderr.decode('utf-8') != "":
+        if proc.stderr.decode("utf-8") != "":
             return proc.stderr.decode("utf-8")
 
-        return proc.stdout.decode('utf-8').strip('\n')
+        return proc.stdout.decode("utf-8").strip("\n")
 
-    @ property
+    @property
     def _meta(self) -> str:
-        proc = run("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'",
-                   shell=True,
-                   capture_output=True)
+        proc = run(
+            "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'",
+            shell=True,
+            capture_output=True,
+        )
 
-        return proc.stdout.decode('utf-8').replace("'", "ʼ").strip('\n')
+        return proc.stdout.decode("utf-8").replace("'", "ʼ").strip("\n")
 
-    @ property
+    @property
     def artist(self) -> str:
-        proc: CompletedProcess = run(f"echo '{self._meta}' | grep -m1 'xesam:artist' -b2 | tail -n 1 | grep -o '\".*\"' | sed 's/\"//g'",
-                                     shell=True,
-                                     capture_output=True)
+        proc: CompletedProcess = run(
+            f"echo '{self._meta}' | grep -m1 'xesam:artist' -b2 | tail -n 1 | grep -o '\".*\"' | sed 's/\"//g'",
+            shell=True,
+            capture_output=True,
+        )
 
         return self.get_proc_output(proc)
 
-    @ property
+    @property
     def song_title(self) -> str:
         proc: CompletedProcess = run(
             f"echo '{self._meta}' | grep -m1 'xesam:title' -b1 | tail -n1 | grep -o '\".*\"' | sed 's/\"//g'",
             shell=True,
-            capture_output=True)
+            capture_output=True,
+        )
 
         return self.get_proc_output(proc)
 
-    @ property
+    @property
     def album(self) -> str:
         proc = run(
             f"echo '{self._meta}' | grep -m1 'xesam:album' -b1 | tail -n1 | grep -o '\".*\"' | sed 's/\"//g'",
             shell=True,
-            capture_output=True)
+            capture_output=True,
+        )
 
         return self.get_proc_output(proc)
 
-    @ property
+    @property
     def playing(self) -> bool:
         play = run(
             "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | grep -o Playing",
             shell=True,
-            capture_output=True).stdout.decode('utf-8')
+            capture_output=True,
+        ).stdout.decode("utf-8")
         if play == "":
             return False
         return True
