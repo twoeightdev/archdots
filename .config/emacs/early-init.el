6ceeb274d5fb7c9c204fd9f60d4f923ec7b57995
twoeightdev <twoeightdev@gmail.com>
2024-05-08 22:56:43 +0800

    Refactor emacs

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,44 +1,32 @@
-;;; early-init.el --- Early Init File -*- lexical-binding: t -*-
+;;; Auto-generated --- DO NOT EDIT -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022-2024 twoeightdev
+;; Copyright (C) 2024 twoeightdev
 
-;; Author: artjeremie <twoeightdev@gmail.com>
+;; Author: twoeightdev <twoeightdev@gmail.com>
 ;; URL: https://github.com/twoeightdev
 
 ;;; Commentary:
 
-;; This file is NOT part of GNU Emacs.  Since Emacs 27, an early configuration
-;; file early-init.el can be provided to handle initialization to be done
-;; before init.el is loaded.
+;; This file is NOT part of GNU Emacs.
 
 ;;; Code:
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-(tooltip-mode -1)
 
 (dolist (var '(default-frame-alist initial-frame-alist))
-  (add-to-list var '(width . (text-pixels . 1125)))
-  (add-to-list var '(height . (text-pixels . 1200)))
-  (add-to-list var '(font . "Inconsolata Nerd Font Mono-14"))
+  (add-to-list var '(font . "Inconsolata Nerd Font-14"))
   (add-to-list var '(alpha . 98))
-  (add-to-list var '(top . -91))
-  (add-to-list var '(left . 913)))
+  (add-to-list var '(background-color . "#1d2021")))
 
-(setq frame-resize-pixelwise t)
-(setq frame-inhibit-implied-resize t)
-
+(setq inhibit-x-resources t)
+(setq inhibit-compacting-font-caches t)
 (setq inhibit-splash-screen t)
-(setq inhibit-startup-buffer-menu t)
-;;(setq frame-title-format '("%b"))
 
 (setq-default load-prefer-newer t)
-(setq-default package-check-signature nil)
 (setq-default native-comp-async-report-warnings-errors 'silent)
-
-;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-;; (load-theme 'art t)
-;;(load-theme 'modus-vivendi)
+(setq-default warning-minimum-level :error)
+(setq-default byte-compile-warnings nil)
 
 ;;; early-init.el ends here
