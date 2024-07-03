7b1dc8826230261bb1a465ce3fbe441477da1361
twoeightdev <twoeightdev@gmail.com>
2024-07-03 21:08:49 +0800

    Fixed indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,10 +3,10 @@ vim.loader.enable()
 
 -- Use pcall to load the modules
 local function _load(module)
-	local success, loaded = pcall(require, module)
-	if success then
-		return loaded
-	end
+    local success, loaded = pcall(require, module)
+    if success then
+        return loaded
+    end
 end
 
 -- Load modules
