cc389737d297ad0ca5110ca52d3ec38be34bfee0
twoeightdev <twoeightdev@gmail.com>
2024-07-25 02:33:00 +0800

    Refactor neovim

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -25,17 +25,17 @@ return {
                 },
                 config = function(_, opts)
                     require("mason").setup(opts)
-                    local mr = require("mason-registry")
+                    local masonreg = require("mason-registry")
                     local function ensure_installed()
                         for _, tool in ipairs(opts.ensure_installed) do
-                            local p = mr.get_package(tool)
+                            local p = masonreg.get_package(tool)
                             if not p:is_installed() then
                                 p:install()
                             end
                         end
                     end
-                    if mr.refresh then
-                        mr.refresh(ensure_installed)
+                    if masonreg.refresh then
+                        masonreg.refresh(ensure_installed)
                     else
                         ensure_installed()
                     end
