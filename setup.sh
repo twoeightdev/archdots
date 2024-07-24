0e1d16913f695438bd2bb06991795125db5847b6
twoeightdev <twoeightdev@gmail.com>
2024-07-25 02:21:12 +0800

    Updated dotfile script setup

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ dmenudir="$HOME/.local/src/archdmenu"
 name="$USER"
 
 menu() {
-    echo -e "\e[1;4;35mUsage:\e[0m \e[1;37m" $me "[OPTION]\e[0m"
+    echo -e "\e[1;4;35mUsage:\e[0m \e[1;37m" "$me" "[OPTION]\e[0m"
     echo " "
     echo -e "\e[1;92maurhelper:\e[0m ............ \e[37myay AUR helper installation.\e[0m"
     echo -e "\e[1;92marchpkg:\e[0m .............. \e[37mARCH packages installation.\e[0m"
@@ -20,10 +20,10 @@ menu() {
 }
 
 aurhelper() {
-    cd /tmp/
+    cd /tmp/ || exit
     curl -LO https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
     tar xvzf yay.tar.gz
-    cd yay
+    cd yay || exit
     makepkg -sci
 }
 
@@ -35,7 +35,7 @@ archpkg() {
     # amd
     pkgs="$pkgs lib32-vulkan-radeon mesa-utils mesa-vdpau vulkan-tools"
     # audio
-    pkgs="$pkgs pipewire pipewire-alsa pipewire-pulse alsa-utils pulsemixer"
+    pkgs="$pkgs pipewire pipewire-alsa pipewire-pulse pulsemixer"
     # fonts
     pkgs="$pkgs libertinus-font noto-fonts noto-fonts-emoji"
     pkgs="$pkgs ttc-iosevka ttc-iosevka-aile ttf-iosevka-nerd ttf-dejavu"
@@ -47,56 +47,54 @@ archpkg() {
     pkgs="$pkgs htop imagemagick libnotify maim man-db mediainfo moreutils"
     pkgs="$pkgs nvtop ntfs-3g picom poppler psutils ripgrep tree unrar unzip"
     pkgs="$pkgs wget yt-dlp zip tesseract tesseract-data-eng tesseract-data-osd"
-    pkgs="$pkgs unclutter polkit cpupower"
-    # needed for neovim
-    pkgs="$pkgs lua-language-server luarocks npm selene bash-language-server"
+    pkgs="$pkgs unclutter polkit cpupower npm"
     # python
     pkgs="$pkgs python-dbus-next python-iwlib python-mpd2 python-pip python-psutil"
-    pkgs="$pkgs python-lsp-server"
     # programs
     pkgs="$pkgs alacritty dunst emacs firefox firefox-tridactyl firefox-ublock-origin"
     pkgs="$pkgs mpc mpd mpv ncmpcpp nsxiv lf newsboat qtile gimp gucharmap steam"
     pkgs="$pkgs transmission-cli zathura zathura-pdf-mupdf spotify-launcher"
     # install all listed pkgs
-    yay --needed --noconfirm -Syu $pkgs
+    yay --needed --noconfirm -Syu "$pkgs"
 }
 
 aurpkg() {
     aurpkgs="tremc-git gtk-theme-arc-gruvbox-git firefox-tridactyl-native"
-    yay --needed --noconfirm --answerclean None --answerdiff None --removemake -S $aurpkgs
+    aurpkgs="$aurpkgs python-pulsectl-asyncio"
+    yay --needed --noconfirm --answerclean None --answerdiff None --removemake -S "$aurpkgs"
 }
 
 dotfiles() {
-    cd ~
-    git clone --bare https://github.com/twoeightdev/archdots.git $HOME/.config/.dots
-    git --git-dir=$HOME/.config/.dots/ --work-tree=$HOME checkout
+    cd ~ || exit
+    git clone --bare https://github.com/twoeightdev/archdots.git "$HOME"/.config/.dots
+    git --git-dir="$HOME"/.config/.dots/ --work-tree="$HOME" checkout
     echo -e "\e[1;31mdone!\e[0m"
 }
 
 clean() {
-    rm -rf /home/$name/.bash_logout
-    rm -rf /home/$name/.bash_profile
-    rm -rf /home/$name/.bashrc
-    rm -rf /home/$name/.zshrc
-    mkdir -p /home/$name/.venv
-    mkdir -p /home/$name/.local/downloads
-    mkdir -p /home/$name/.local/drafts
-    mkdir -p /home/$name/.local/screenshot
-    mkdir -p /home/$name/.local/src
-    mkdir -p /home/$name/.local/testfiles
-    mkdir -p /home/$name/.local/torrent
+    rm -rf /home/"$name"/.bash_logout
+    rm -rf /home/"$name"/.bash_profile
+    rm -rf /home/"$name"/.bashrc
+    rm -rf /home/"$name"/.zshrc
+    mkdir -p /home/"$name"/.venv
+    mkdir -p /home/"$name"/.local/downloads
+    mkdir -p /home/"$name"/.local/drafts
+    mkdir -p /home/"$name"/.local/screenshot
+    mkdir -p /home/"$name"/.local/src
+    mkdir -p /home/"$name"/.local/testfiles
+    mkdir -p /home/"$name"/.local/torrent
     echo -e "\e[1;31mdone!\e[0m"
 }
 
 dmenu() {
-    cd $srcdir
+    cd "$srcdir" || exit
     git clone https://github.com/twoeightdev/archdmenu.git
-    cd $dmenudir
+    cd "$dmenudir" || exit
     make && sudo make install
 }
 
 startpage() {
-    cd $srcdir
+    cd "$srcdir" || exit
     git clone https://github.com/twoeightdev/startpage.git
 }
 
