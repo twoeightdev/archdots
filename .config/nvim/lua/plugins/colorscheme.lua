dbfdbc8527117af34ea60c2b0ce1769707de8275
twoeightdev <twoeightdev@gmail.com>
2024-08-10 07:53:04 +0800

    Disabled transparency

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,7 +9,7 @@ return {
             light = "latte",
             dark = "mocha",
         },
-        transparent_background = true,
+        transparent_background = false,
         no_italic = false,
         no_bold = false,
         no_underline = false,
