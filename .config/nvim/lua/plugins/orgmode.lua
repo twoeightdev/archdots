d8a8be81dfda080ad8111bef5aedfdaf89d53b34
twoeightdev <twoeightdev@gmail.com>
2024-08-10 22:25:42 +0800

    Changed org files default path

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -3,8 +3,8 @@ return {
     event = "VeryLazy",
     ft = { "org" },
     opts = {
-        org_agenda_files = "~/.local/org/agenda/**/*",
-        org_default_notes_file = "~/.local/org/agenda/refile.org",
+        org_agenda_files = "~/.local/src/orgnotes/agenda/**/*",
+        org_default_notes_file = "~/.local/src/orgnotes/agenda/refile.org",
         mappings = {
             org_return_uses_meta_return = true,
         },
