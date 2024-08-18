547e7a4cd74b3778d5cac41ef2c8526f6bbc1a2f
twoeightdev <twoeightdev@gmail.com>
2024-08-18 19:36:51 +0800

    Added statuscol for better fold signs

---
+++ .config/nvim/lua/plugins/statuscol.lua
@@ -0,0 +1,50 @@
+return {
+    "luukvbaal/statuscol.nvim",
+    branch = "0.10",
+    opts = function()
+        local builtin = require("statuscol.builtin")
+
+        return {
+            bt_ignore = { "nofile", "terminal" },
+            segments = {
+                {
+                    sign = {
+                        name = { ".*" },
+                        text = { ".*" },
+                    },
+                    click = "v:lua.ScSa",
+                },
+                {
+                    text = { builtin.lnumfunc },
+                    click = "v:lua.ScLa",
+                },
+                {
+                    sign = {
+                        namespace = { "gitsigns" },
+                        colwidth = 1,
+                        wrap = true,
+                        foldclosed = true,
+                    },
+                    condition = {
+                        function(args)
+                            return vim.wo[args.win].number
+                                or vim.b[args.buf].gitsigns_statusr
+                        end,
+                    },
+                    click = "v:lua.ScSa",
+                },
+                {
+                    text = {
+                        function(args)
+                            args.fold.close = ""
+                            args.fold.open = ""
+                            args.fold.sep = "▕"
+                            return builtin.foldfunc(args)
+                        end,
+                    },
+                    click = "v:lua.ScFa",
+                },
+            },
+        }
+    end,
+}
