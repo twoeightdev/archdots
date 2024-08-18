46435d4eee45ce6b302f3397705f1da649a15aba
twoeightdev <twoeightdev@gmail.com>
2024-08-18 15:02:54 +0800

    Set global markdownlint configuration

--- .config/nvim/lua/plugins/linter.lua
+++ .config/nvim/lua/plugins/linter.lua
@@ -13,35 +13,21 @@ return {
         lint.linters_by_ft = opts.linters_by_ft
 
         lint.linters.markdownlint.args = {
-            "--disable",
-            "MD013",
-            "MD004",
-            "MD007",
-            "MD030",
+            "--config",
+            os.getenv("HOME") .. "/.config/nvim/.markdownlint.yaml",
             "--",
         }
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
         vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+            desc = "Linters",
             group = lint_augroup,
             callback = function()
                 local client = vim.lsp.get_clients({ bufnr = 0 })[1] or {}
-                require("lint").try_lint(nil, { cwd = client.root_dir })
-                -- require("lint").try_lint()
+                lint.try_lint(nil, { cwd = client.root_dir })
+                -- lint.try_lint()
             end,
         })
-
-        -- vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-        --     callback = function()
-        --         local nvim_lint = require("lint")
-        --         local linters = nvim_lint.linters_by_ft[vim.bo.filetype] or {}
-        --
-        --         for _, linter in ipairs(linters) do
-        --             local ns = nvim_lint.get_namespace(linter)
-        --             vim.diagnostic.reset(ns, 0)
-        --         end
-        --     end,
-        -- })
     end,
 }
