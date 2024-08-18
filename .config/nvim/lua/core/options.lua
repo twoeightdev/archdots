34fecc44cd89349238667b4b7d07c22de420428b
twoeightdev <twoeightdev@gmail.com>
2024-08-18 14:50:57 +0800

    Updated defaults

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,109 +1,81 @@
-local defaults = {
-    -- Pane splitting
-    splitright = true,
-    splitbelow = true,
-    -- Searching
-    smartcase = true,
-    ignorecase = true,
-    -- Terminal truecolor support
-    termguicolors = true,
-    -- Use system clipboard -> xclip
-    clipboard = "unnamedplus",
-    -- Old vim status
-    showmode = false,
-    -- Line numbers
-    number = true,
-    relativenumber = true,
-    numberwidth = 4,
-    -- Tabs
-    tabstop = 4,
-    expandtab = true,
-    smartindent = true,
-    shiftwidth = 4,
-    shiftround = true,
-    -- Folds
-    foldlevel = 99,
-    foldlevelstart = 99,
-    foldcolumn = "1",
-    -- Update time
-    updatetime = 200,
-    -- Persistent undo
-    undofile = true,
-    -- Max number of undo changes
-    undolevels = 10000,
-    -- Tabline
-    showtabline = 0,
-    -- Mouse support disable
-    mouse = "",
-    -- Swapfile
-    swapfile = false,
-    -- Scrolloff
-    scrolloff = 5,
-    sidescrolloff = 5,
-    -- Wrapping
-    wrap = false,
-    -- Show invisible characters
-    list = true,
-    -- Statusline only at the bottom
-    laststatus = 0,
-    -- Confirm save changed on exit
-    confirm = true,
-    -- Hide markup for bold and italics, but not markers with substitutions
-    conceallevel = 2,
-    -- Enabled for orgmode-nvim
-    concealcursor = "nc",
-    -- Use ripgrep as grep program for neovim
-    grepprg = "rg --vimgrep",
-    grepformat = "%f:%l:%c:%m",
-    -- Autowrite
-    autowrite = true,
-    -- Keep cursor at the same line when opening a split
-    splitkeep = "screen",
-    -- Completions
-    completeopt = "menu,menuone,noselect",
-    wildmode = "longest:full,full",
-    -- Display 10 items in a completion menu
-    pumheight = 10,
-    -- Keytimeout to 300ms
-    timeoutlen = 300,
-    -- Window configuration
-    winwidth = 5,
-    winminwidth = 5,
-    equalalways = false,
-    -- Signcolumn
-    signcolumn = "yes",
-    -- Formatting
-    formatoptions = "jcroqlnt",
-    -- Put cursor at the start of the line for large jumps
-    startofline = true,
-    -- Allow cursor to move where there is no text in visual block mode
-    virtualedit = "block",
-    -- Partial command letters below statusline
-    showcmd = false,
-    -- Enable lazy redraw for performance
-    lazyredraw = true,
-}
-
-for key, value in pairs(defaults) do
-    vim.opt[key] = value
-end
-
 local opt = vim.opt
 local g = vim.g
 
+-- New
+-- opt.showcmdloc = "statusline"
+opt.showbreak = "↪ "
+opt.listchars =
+    { tab = "│ ", lead = "·", trail = "·", precedes = "«", extends = "»" }
+opt.laststatus = 3
+opt.spelllang = { "en" }
+-- Line numbers
+opt.number = true
+opt.numberwidth = 5
+-- Tabs
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.shiftround = true
+opt.expandtab = true
+opt.smartindent = true
+-- System clipboard
+opt.clipboard = "unnamedplus"
+-- Swap file
+opt.swapfile = false
+-- Terminal truecolor support
+opt.termguicolors = true
+-- Disable mouse support
+opt.mouse = ""
+-- Disable text wrapping
+opt.wrap = false
+-- Don't tabline
+opt.showtabline = 0
+-- Search
+opt.smartcase = true
+opt.ignorecase = true
+-- Persistent undo
+opt.undofile = true
+opt.undolevels = 10000
+-- Split
+opt.splitright = true
+opt.splitbelow = true
+-- Folds
+opt.foldcolumn = "3"
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+-- Scrolloff
+opt.scrolloff = 15
+opt.sidescrolloff = 15
+-- Show invisible characters
+opt.list = true
+-- Hide markup for bold and italics, but not markers with substitutions
+opt.conceallevel = 2
+-- Use ripgrep as grep program
+opt.grepprg = "rg --vimgrep"
+opt.grepformat = "%f:%l:%c:%m"
+-- Key timeout to 300ms
+opt.timeoutlen = 300
+-- Update time
+opt.updatetime = 200
+-- Confirm save changed on exit
+opt.confirm = true
+-- Keep cursor at the same line when opening a split
+opt.splitkeep = "screen"
+-- Window configuration
+opt.winwidth = 5
+opt.winminwidth = 5
+opt.equalalways = false
+-- Formatting
+opt.formatoptions = "jcroqlnt"
+-- Completion
+opt.completeopt = "menu,menuone,noselect"
+opt.wildmode = "longest:full,full"
+opt.pumheight = 10
+-- Allow cursor to move where there is no text in visual block mode
+opt.virtualedit = "block"
+-- Partial command letters bellow statusline
+opt.showcmd = false
 -- Shortmess
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
-
--- Fill chars
-opt.fillchars = {
-    foldopen = "",
-    foldclose = "",
-    fold = " ",
-    foldsep = " ",
-    diff = "╱",
-    eob = " ",
-}
-
 -- Session save options
 opt.sessionoptions = {
     "buffers",
@@ -117,13 +89,12 @@ opt.sessionoptions = {
     "winpos",
     "resize",
 }
+opt.autowrite = true
 
--- Disable LSP logging
+-- Disable lsp logging
 vim.lsp.set_log_level(vim.lsp.log_levels.OFF)
 
--- g.python3_host_prog = "~/.venv/work/mainpy/bin/python"
-
--- Disabled provider warnings in checkhealth
+-- Disabled provider checkhealth warnings
 g.loaded_node_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
