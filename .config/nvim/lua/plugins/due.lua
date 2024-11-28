2eca642029298f2ff44fc31adbff130851addb32
twoeightdev <twoeightdev@gmail.com>
2024-11-29 04:02:11 +0800

    Due plugins

---
+++ .config/nvim/lua/plugins/due.lua
@@ -0,0 +1,13 @@
+return {
+    "nfrid/due.nvim",
+    opts = {
+        draw = 0,
+        clean = 0,
+        redraw = 0,
+        async_update = 0,
+        pattern_start = "[",
+        pattern_end = "]",
+        use_clock_time = true,
+        default_due_time = "noon",
+    },
+}
