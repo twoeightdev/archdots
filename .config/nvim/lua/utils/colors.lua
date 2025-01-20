291cf29cc6741ac0f0a077c366b25c62ef44897c
twoeightdev <twoeightdev@gmail.com>
2025-01-20 13:26:43 +0800

    Modular colors.lua

---
+++ .config/nvim/lua/utils/colors.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+-- Catppuccin mocha
+M.cm = {
+    c01 = "#1e1e2e",
+    c02 = "#f38ba8",
+    c03 = "#a6e3a1",
+    c04 = "#f9e2af",
+    c05 = "#89b4fa",
+    c06 = "#f5c2e7",
+    c07 = "#94e2d5",
+    c08 = "#cba6f7",
+    c09 = "#fab387",
+    c10 = "#b4befe",
+    c11 = "#74c7ec",
+    c12 = "#f5e0dc",
+}
+
+-- Kanagawa dragon
+M.kd = {
+    c01 = "#181616",
+    c02 = "#c4746e",
+    c03 = "#8a9a7b",
+    c04 = "#c4b28a",
+    c05 = "#8ba4b0",
+    c06 = "#a292a3",
+    c07 = "#8ea4a2",
+    c08 = "#957fb8",
+    c09 = "#b6927b",
+    c10 = "#9cabca",
+    c11 = "#7fb4ca",
+    c12 = "#a99c8b",
+}
+
+return M
+
+-- Last Modified: Sun, 19 Jan 2025 01:49:05 AM
